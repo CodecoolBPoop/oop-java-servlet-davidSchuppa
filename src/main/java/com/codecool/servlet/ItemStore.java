adb743d2e9121035d610ad4bc7f906c171430ec1
David Schuppa <d.schuppa@gmail.com>
2018-10-10 11:14:15 +0200

    made ItemStore an abstract class, started implementation of core functions

--- src/main/java/com/codecool/servlet/ItemStore.java
+++ src/main/java/com/codecool/servlet/ItemStore.java
@@ -1,4 +1,27 @@
 package com.codecool.servlet;
 
-public class ItemStore {
+
+/*
+contains a static List (ArrayList or LinkedList) of Items. It should provide static functions as well, like add and remove.
+The business logic is not that important, adding multiple Items with the same id is valid use case.
+Pro version: store it in a session instead of a static list!
+ */
+
+import java.util.ArrayList;
+
+public abstract class ItemStore {
+    private static ArrayList<Item> items = new ArrayList<>();
+
+    private static void add(Item item){
+        items.add(item);
+    }
+
+    private static Item get(String name){
+        for(Item item : items){
+            if(item.getName().equals(name)){
+                return item;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/codecool/servlet/ShoppingCartServlet.java
+++ src/main/java/com/codecool/servlet/ShoppingCartServlet.java
@@ -1,4 +1,11 @@
 package com.codecool.servlet;
 
-public class ShoppingCartServlet {
+
+/*
+This should list the content of ItemStore's static list.
+It should render a HTML list or a table with the items in it.
+It should render a Sum of Price field as well.
+ */
+
+public class ShoppingCartServlet extends ItemStore {
 }
