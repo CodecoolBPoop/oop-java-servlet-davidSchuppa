b6427e53565b2b4e5677fe9a6c1cdf44a80e1c91
David Schuppa <d.schuppa@gmail.com>
2018-10-10 11:13:16 +0200

    implemented Item class, its constructor and getName getter

---
+++ src/main/java/com/codecool/servlet/Item.java
@@ -0,0 +1,23 @@
+package com.codecool.servlet;
+
+
+/*
+ a simple Java class with id (generated integer), name (string) and price (float or double) properties.
+ */
+
+public class Item {
+    private static int instanceCounter = 0;
+    private int id;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.id = instanceCounter++;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
